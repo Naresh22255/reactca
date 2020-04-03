10bf05f528df52c825914dabecafe3e07b974d7a
naresh22255 <naresh22255@gmail.com>
2020-04-03 14:07:38 +0200

    Created Home and contact components

--- gameca/src/App.js
+++ gameca/src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
+
+import { Home } from "./components/Home";
+import { Contact } from "./components/Contact";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
+      <Home />
+      <Contact />
       <h1>Hello React!</h1>
+      <Button variant="primary">Primary</Button>{" "}
+      <Button variant="outline-warning">Warning</Button>{" "}
     </div>
   );
 }
---
+++ gameca/src/components/Contact.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export class Contact extends Component {
+  render() {
+    return (
+      <div className="mt-4 d-flex justify-content-center">
+        <h2>Welcome to the Contact Page</h2>
+      </div>
+    );
+  }
+}
